966844bc404648c7b8764fe36be10478429fa434
BONAM0RS <87779923+BONAM0RS@users.noreply.github.com>
2022-10-21 13:16:22 +0300

    Change load image folder

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main()
 	//            0  1  2  3  4  5  6  7  8  9     10              11
 
 	Texture texture;
-	texture.loadFromFile("F:\\MinesweeperImage\\tiles.jpg");
+	texture.loadFromFile("Images\\tiles.png");
 
 	Sprite sprite(texture);
 
